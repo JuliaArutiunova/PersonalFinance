f371cd669aeeb69e3bebd0b4409b4d51ab74a134
Julia Arutiunova <j.arutiunova1@gmail.com>
2025-02-17 16:17:16 +0300

    chore: add NoArgsConstructor to TokenInfoDto

--- pf-common-lib/src/main/java/by/it_academy/lib/dto/TokenInfoDto.java
+++ pf-common-lib/src/main/java/by/it_academy/lib/dto/TokenInfoDto.java
@@ -2,8 +2,10 @@ package by.it_academy.lib.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class TokenInfoDto {
     private String id;
