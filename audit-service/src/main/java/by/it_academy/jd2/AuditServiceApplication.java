0297633c24e5513dbcf9f8e285c593031a180063
Julia Arutiunova <j.arutiunova1@gmail.com>
2025-02-27 13:37:11 +0300

    chore: Add project structure for audit-service

---
+++ audit-service/src/main/java/by/it_academy/jd2/AuditServiceApplication.java
@@ -0,0 +1,21 @@
+package by.it_academy.jd2;
+
+
+import by.it_academy.jd2.config.property.JWTProperty;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EnableConfigurationProperties({JWTProperty.class})
+public class AuditServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditServiceApplication.class, args);
+	}
+
+}
