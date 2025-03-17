8668e0c5f82e61a87a6fe66b4f016c108387ea38
Julia Arutiunova <j.arutiunova1@gmail.com>
2025-03-17 13:19:07 +0300

    feat: add audit-service FeignClient interface in classifier-service

---
+++ classifier-service/src/main/java/by/it_academy/jd2/service/client/AuditClient.java
@@ -0,0 +1,12 @@
+package by.it_academy.jd2.service.client;
+
+import by.it_academy.lib.dto.ActionInfoDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "audit-service", url = "${client.audit-service.url}")
+public interface AuditClient {
+    @PostMapping("/audit_data/audit_create")
+    void createAudit(@RequestBody ActionInfoDto actionInfoDto);
+}
